df34b7084a92fe1b891f57a25999102a844133b0
wozsun <master@wozsun.com>
2023-06-28 23:09:27 +0800

    ✨ feat: 增加杂项分类并添加到导航栏

--- docs/.vuepress/bar.ts
+++ docs/.vuepress/bar.ts
@@ -1,11 +1,13 @@
-import { navbar,sidebar } from "vuepress-theme-hope";
+import { navbar, sidebar } from "vuepress-theme-hope";
+
+// 图标浏览https://remixicon.com/
 
 export const Navbar = navbar([
     { text: "首页", icon: "home-3-line", link: "/" },
+    { text: "杂项", icon: "file-copy-2-line", link: "/misc/" },
     { text: "博客", icon: "gps-line", link: "https://www.wozsun.com" },
 ]);
 
 export const Sidebar = sidebar({
-    //自动侧边栏目录
-    // "/java/": "structure",
+    "/misc/": "structure",
 });
---
+++ docs/misc/README.md
@@ -0,0 +1,6 @@
+---
+title: 杂项
+icon: file-copy-2-line
+index: false
+pageInfo: []
+---
